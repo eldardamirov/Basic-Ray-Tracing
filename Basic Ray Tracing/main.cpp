a3de01043a340e1bdfa6f5427135b2f405f3ab24
eldardamirov <eldardamirov75@mail.ru>
2019-01-22 19:33:58 +0300

    Initial Commit

---
+++ Basic Ray Tracing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Basic Ray Tracing
+//
+//  Created by Эльдар Дамиров on 22.01.2019.
+//  Copyright © 2019 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
