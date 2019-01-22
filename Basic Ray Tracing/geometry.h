4269b1938606b9ce676014f267db06aa6ecac104
eldardamirov <eldardamirov75@mail.ru>
2019-01-22 19:54:00 +0300

    geometry.h file added.

--- Basic Ray Tracing/geometry.h
+++ Basic Ray Tracing/geometry.h
@@ -1,19 +1,13 @@
 //
-//  main.cpp
+//  geometry.h
 //  Basic Ray Tracing
 //
 //  Created by Эльдар Дамиров on 22.01.2019.
 //  Copyright © 2019 Эльдар Дамиров. All rights reserved.
 //
 
-#include <limits>
-#include <cmath>
-#include <iostream>
-#include <fstream>
-#include <vector>
+#ifndef geometry_h
+#define geometry_h
 
-int main() 
-    {
-    
-    return 0;
-    }
+
+#endif /* geometry_h */
