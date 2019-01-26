b6ecf00391c87c4eb9114366381ce6746b3c7f7c
eldardamirov <eldardamirov75@mail.ru>
2019-01-26 23:26:01 +0300

    render.h file added.

---
+++ Basic Ray Tracing/render.h
@@ -0,0 +1,13 @@
+//
+//  render.h
+//  Basic Ray Tracing
+//
+//  Created by Эльдар Дамиров on 26.01.2019.
+//  Copyright © 2019 Эльдар Дамиров. All rights reserved.
+//
+
+#ifndef render_h
+#define render_h
+
+
+#endif /* render_h */
