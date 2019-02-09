a7f7dde8ce95dc4037c7ab719d924515e1853f48
eldardamirov <eldardamirov75@mail.ru>
2019-02-09 22:58:03 +0300

    Basic render function finished.

--- Basic Ray Tracing/render.h
+++ Basic Ray Tracing/render.h
@@ -30,9 +30,39 @@
 //    
 //    }
 
+#include <fstream>
+#include <limits.h>
 
+const size_t width = 1024;
+const size_t height = 768;
 
-
+void render()
+    {
+    std::vector <Vec3f> frameBuffer ( width * height );
+    
+    for ( size_t currentY = 0; currentY < height; currentY++ )
+        {
+        for ( size_t currentX = 0; currentX < width; currentX++ )
+            {
+            frameBuffer [ currentY + ( currentX * width ) ] = Vec3f ( ( currentY / static_cast <double> ( height ) ), ( currentX / static_cast <double> ( width ) ), 0 );
+            }
+        }
+        
+    std::ofstream outputStream;
+    outputStream.open ( "./out.ppm" );
+    
+    outputStream << "P6\n" << width << " " << height << "\n255\n";
+    for ( size_t i = 0; i < ( height * width ); i++ )
+        {
+        for ( size_t j = 0; j < 3; j++ )
+            {
+            outputStream << static_cast <char> ( 255 * std::max ( 0.f, std::min ( 1.f, frameBuffer [ i ] [ j ] ) ) );
+            }
+        }
+    
+    outputStream.close();
+    }
+    
 
 
 #endif /* render_h */
